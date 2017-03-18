f09f2f19f94418f1a6d4e2d839d7a7388147a981
nsedarati <n.sedarati@gmail.com>
2017-03-18 12:18:32 +0800

    todo_list view done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :todo_lists
   enum access_level: [:user, :admin]
   # has_many :todo_lists
   has_many :authentications, :dependent => :destroy
